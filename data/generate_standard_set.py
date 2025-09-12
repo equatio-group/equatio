83092a936216a2727805c785d01314c5deca0415
Lennart <lennart.hueckstaedt@studium.uni-hamburg.de>
2025-09-12 17:58:14 +0200

    added generate_standard_set.py to create standard equation set for the game

---
+++ data/generate_standard_set.py
@@ -0,0 +1,12 @@
+#should create a standard equation set that can be used for the game
+from pathlib import Path
+
+from src.equatio.equation_set import EquationSet
+
+DATA_PATH = Path(__file__).parents[1]
+SPRITE_DIR = DATA_PATH / 'sprites'
+JSON_DIR = DATA_PATH / 'data'
+
+STANDARD_SET = EquationSet.from_json(JSON_DIR / 'standard_set.json')
+
+print(STANDARD_SET)
